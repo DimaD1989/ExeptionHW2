396914871dbda091a56e08d442980afb55449120
DimaD1989 <109169005+DimaD1989@users.noreply.github.com>
2023-04-17 20:37:46 +0300

    All
    this is all

--- src/Main.java
+++ src/Main.java
@@ -18,3 +18,63 @@ public  class Main {
         System.out.println("Вы ыыули: \n" + (num));
         return num;
     }
+    /**
+     * Задача 2.1
+     * Исправить код, добавил массив
+     */
+    public static void task2() {
+        try {
+            int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 10};
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+
+    /**
+     * Задача 2.1 исправить код
+     */
+    public static void task2_2() {
+        try {
+            int a = 33;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(5, 12);
+            int[] abc = {1, 2};
+            abc[3] = 2;
+
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+    }
+
+    /**
+     * Задача 3
+     *  Разработайте программу,
+     *  которая выбросит Exception,
+     *  когда пользователь вводит пустую строку.
+     *  Пользователю должно показаться сообщение,
+     *  что пустые строки вводить нельзя.
+     * @return возвращает введённую строку
+     */
+    public static String task3() {
+        System.out.println("\nВведите сообщение : ");
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        System.out.println("Вы ввели: \n" + (str));
+        if(str.isEmpty())
+            throw new IllegalArgumentException("Пустые строки вводить нельзя!");
+        return str;
+    }
+
+}
