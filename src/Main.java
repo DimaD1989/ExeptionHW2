8f4564c066d42526ce13f5bc3e5eb3d7d8300d89
DimaD1989 <d_d1989sv@mail.ru>
2023-04-17 19:50:28 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+//Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), и возвращает введенное значение.
+// Ввод текста вместо числа не должно приводить к падению приложения, вместо этого, необходимо повторно запросить
+// у пользователя ввод данных
+
+import java.util.Scanner;
+
+public  class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
+
+    public static floatNum(){
+        System.out.println("\nВаедите число через запятую: ");
+        Scanner sc = new Scanner(System.in);
+        float num = sc.nextFloat();
+        System.out.println("Вы ыыули: \n" + (num));
+        return num;
+    }
